bf6f1122259368e84e20dec0f274bf98c0652697
AnVai <kensan010204@gmail.com>
2024-11-18 22:28:07 +0700

    Update login.component.ts

--- Front-end_Manager/Src/App/Components/login/login.component.ts
+++ Front-end_Manager/Src/App/Components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -8,15 +9,23 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private http: HttpClient) {}
 
   onLogin(): void {
     const emailInput = (document.getElementById('email') as HTMLInputElement).value;
     const passwordInput = (document.getElementById('password') as HTMLInputElement).value;
 
     if (emailInput && passwordInput) {
-      alert('Đăng nhập thành công!');
-      this.router.navigate(['/dashboard']);
+      this.http.post('http://localhost:5057/api/User_info/login', {username: emailInput, password: passwordInput,})
+        .subscribe({
+          next: () => {
+            alert('Đăng nhập thành công!');
+            this.router.navigate(['/dashboard']);
+          },
+          error: () => {
+            alert('Email hoặc mật khẩu không chính xác!');
+          },
+        });
     } else {
       alert('Vui lòng nhập email và mật khẩu!');
     }
