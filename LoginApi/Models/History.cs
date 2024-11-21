887bff500413d8722d3270371c75b3e33a519bc2
ImWalk6 <167219153+ImWalk6@users.noreply.github.com>
2024-11-21 13:01:54 +0700

    Update History.cs
    Change PrinterID from string to int, add 2 class ( printer report for grouping printer id and pages printed and monthly report)

--- LoginApi/Models/History.cs
+++ LoginApi/Models/History.cs
@@ -14,9 +14,23 @@ namespace LoginApi.Models
         public int UserId { get; set; }
         public DateTime PrintDate { get; set; }
         public int PagesPrinted { get; set; }
-        public string PrinterId { get; set; }
+        public int PrinterId { get; set; }
         public string FileFormats { get; set; }
         public string Title { get; set; }
+    }
+    
+    public class PrinterReport
+    {
+        public int PrinterId { get; set; }
+        public int PagesPrinted { get; set; }
+    }
+
+    public class MonthlyReport
+    {
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public List<PrinterReport> PrinterReports { get; set; }
+        public decimal TotalPayments { get; set; }
     }
 
 }
